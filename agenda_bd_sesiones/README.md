062cd45a77146cb0764acbbb0c6f476cf3de6ee5
jivanrod <jivanrod@gmail.com>
2024-12-04 17:38:12 +0100

    Initial commit

---
+++ agenda_bd_sesiones/README.md
@@ -0,0 +1,22 @@
+# agenda_bd
+ Programa una aplicación de gestión de una agenda de contactos.
+La aplicación permite realizar las siguientes operaciones:
+1. Inserción de contactos
+Se puede rellenar los campos de nombre y teléfono para insertar un nuevo contacto en la agenda.
+Los campos de nombre y telefono podrán ser validados con expresiones regulares para que cumplan ciertas normas de 
+longitudes y caracteres permitidos. Por ejemplo, el nombre puede tener una longitud entre 2 y 25 caracteres y el telefono
+ entre 9 y 15 digitos. 
+2. Borrado de contactos
+Para borrar un contacto se podrá pulsar sobre la papelera que aparece al lado del contacto.
+3. Vaciar la agenda
+Se puede solicitar a la aplicación que se vacíe la agenda completa.
+
+Desarrolla la aplicación usando sesiones PHP donde sea conveniente.
+A continuación, se muestran distintas imágenes de la apariencia de la aplicación.
+
+
+![agenda_vacia](/img/agendadb_vacia.png)
+
+
+![agenda](/img/agendadb.png)
+
---
+++ agenda_bd_sesiones/bd/agenda.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-12-2024 a las 19:33:56
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `agenda`
+--
+CREATE DATABASE IF NOT EXISTS `agenda` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci;
+USE `agenda`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contactos`
+--
+
+DROP TABLE IF EXISTS `contactos`;
+CREATE TABLE IF NOT EXISTS `contactos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `telefono` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
